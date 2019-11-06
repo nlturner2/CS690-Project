64e7f28f85b204afe09941f8cbc57332582d5653
mohanad-alsharef <mmalsharef@bsu.edu>
2019-11-05 20:34:16 -0500

    Bug fix...

--- WindowsFormsApp1/Team Dashboard.cs
+++ WindowsFormsApp1/Team Dashboard.cs
@@ -24,7 +24,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Close teamdashboard and shree is blind
+            //Close teamdashboard
             Close();
         }
 
