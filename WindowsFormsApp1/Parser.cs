835574a698dd1f97ba47943ead63686a2f0d5956
wenjunshi <wenjunshi6@gmail.com>
2019-11-15 19:33:32 -0500

    add parser class for parsing data

---
+++ WindowsFormsApp1/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace WindowsFormsApp1
+{
+    public class Parser
+    {
+        private string teamURL;
+        public Parser()
+        {
+           
+            
+        }
+    }
+}
