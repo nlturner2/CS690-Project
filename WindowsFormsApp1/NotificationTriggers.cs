e29498e57748ac248c94c162ea280fbcb0159e8c
mohanad-alsharef <mmalsharef@bsu.edu>
2019-11-18 20:21:36 -0500

    Create Trigger functions

--- WindowsFormsApp1/NotificationTriggers.cs
+++ WindowsFormsApp1/NotificationTriggers.cs
@@ -12,5 +12,48 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        public void CommitTrigger(TeamMembers aMember, int lastCommit,int days)
+        {
+            if (days<lastCommit)
+            {
+                aMember.CommitNotification = true;
+                //more code to make it apper
+            }
+            else
+            {
+                aMember.CommitNotification = false;
+                //dissmiss notification
+            }
+        }
+        public void MeetingTrigger(Team aTeam, int lastMeeting, int days)
+        {
+            if (days < lastMeeting)
+            {
+                aTeam.MeetingNotification = true;
+                //more code to make it apper
+            }
+            else
+            {
+                aTeam.MeetingNotification = false;
+                //dissmiss notification
+            }
+        }
+        /**
+        public Boolean ProgressTrigger(Array theMembers[])
+        {
+            Boolean check = false;
+            for ( int i = theMembers.Length; TeamMembers aMember in theMembers[])
+
+            {
+                if (aMember.CommitNotification)
+                {
+                    theMembers[].CommitNotification = true;
+
+                }
+            }
+            return check;
+        }
+    */
     }
 }
